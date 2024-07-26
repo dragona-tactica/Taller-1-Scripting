ace993cd3c758b1ed2b3ab2b3e5cad5a8adac9b3
Vanesa Diosa <143772569+VanDiosa@users.noreply.github.com>
2024-07-25 23:27:42 -0500

    Update Firmas.C
    Funcion 2

--- Firmas.C
+++ Firmas.C
@@ -5,12 +5,20 @@ class Program
     static void Main(string[] arg)
     {
         Saludar();
+        
+        ImprimirNombre("Vanesa");
     }
 
-    //Funcion sin parametros
+    //Funcion 1: sin parametros y sin retorno
     static void Saludar()
     {
         Console.WriteLine("¡Hola!");
+    }
+    
+    // Funcion 2: con parametros y sin retorno
+    static void ImprimirNombre(string nombre)
+    {
+        Console.WriteLine($"Nombre: {nombre}");
     }
 
 }
